16a8aefed27d46cc4c8651612ca8ccf8542c7dc6
Rustam <jvmusin@gmail.com>
2024-08-22 16:35:01 +0200

    Simplify DriveResetInfo

--- src/main/kotlin/DriveResetInfo.kt
+++ src/main/kotlin/DriveResetInfo.kt
@@ -1,32 +1,17 @@
 package io.github.jvmusin
 
-import com.google.gson.JsonObject
-import java.nio.file.FileVisitResult
-import kotlin.io.path.*
-
 object DriveResetInfo {
-    private val log = getLogger()
-
-    @OptIn(ExperimentalPathApi::class)
     @JvmStatic
     fun main(args: Array<String>) {
-        log.info("Resetting info about files on drive")
-        downloadedPath.visitFileTree {
-            onVisitFile { file, _ ->
-                if (file.extension == "json" && file.name != "_folder.json") {
-                    val text = file.readText()
-                    val json = gson().fromJson(text, JsonObject::class.java)
-                    val removed = json.remove("driveInfo")
-                    if (removed != null) {
-                        file.writeText(gson().toJson(json))
-                        log.info("Removed info about drive for file $file")
-                    } else {
-                        log.info("No info found, skipping file $file")
-                    }
+        object : ProcessAllFiles("resetting info about files on Google Drive") {
+            override fun visitFile(location: FileLocation) {
+                if (location.json.driveFileId == null) {
+                    log("No info about this file on drive, skipping")
+                } else {
+                    location.updateJson { driveFileId = null }
+                    log("Reset info about this file (had id ${location.driveFileId})")
                 }
-                FileVisitResult.CONTINUE
             }
-        }
-        log.info("Done resetting drive info")
+        }.run()
     }
 }
