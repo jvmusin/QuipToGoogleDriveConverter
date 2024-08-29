004cfc934ca3d599bf8edfbf2d871af6dca843fb
Rustam <jvmusin@gmail.com>
2024-08-29 15:25:06 +0200

    Cleanup QuipUserRepository

--- src/main/kotlin/QuipUserRepository.kt
+++ src/main/kotlin/QuipUserRepository.kt
@@ -2,6 +2,7 @@ package io.github.jvmusin
 
 import com.google.gson.JsonObject
 import kenichia.quipapi.QuipUser
+import java.nio.file.Path
 import java.nio.file.Paths
 import kotlin.io.path.exists
 import kotlin.io.path.readText
@@ -16,36 +17,6 @@ class QuipUserRepository {
         val emails: List<String>,
         val name: QuipUserName,
     )
-
-    private fun QuipUserResource.withLowercaseId() = copy(id = id.lowercase())
-    private val extraEmailsPath = Paths.get("quip_emails_extra.json")
-
-    private fun getEmailByUserId(): Map<String, QuipUserResource> {
-        val userIdToEmail = gson()
-            .fromJson(Paths.get("quip_users_scim.json").readText(), JsonObject::class.java)
-            .getAsJsonArray("Resources")
-            .map { gson().fromJson(it, QuipUserResource::class.java).withLowercaseId() }
-            .associateBy { it.id.lowercase() }
-        val extraUserIdToEmail = if (extraEmailsPath.exists()) {
-            gson()
-                .fromJson(extraEmailsPath.readText(), JsonObject::class.java)
-                .asJsonObject
-                .entrySet()
-                .associate { entry ->
-                    val id = entry.key.lowercase()
-                    val nameAndEmail = entry.value.asString
-                    require(id !in userIdToEmail) { "Repeating id $id" }
-                    val parts = nameAndEmail.split(":")
-                    require(parts.size <= 2) { "Invalid name and email format for $id: $nameAndEmail" }
-                    val name = parts[0]
-                    val email = parts.getOrNull(1) ?: ""
-                    id to QuipUserResource(id, listOfNotNull(email), QuipUserName(name))
-                }
-        } else {
-            emptyMap()
-        }
-        return userIdToEmail + extraUserIdToEmail
-    }
 
     private val userIdToResource = getEmailByUserId()
     private val quipCache = hashMapOf<String, QuipUser>()
@@ -63,7 +34,7 @@ class QuipUserRepository {
         }
     }
 
-    fun getUserEmails(id: String): List<String>? {
+    private fun getUserEmails(id: String): List<String>? {
         // QuipUser returns users without emails, so don't bother checking it in the quipCache
         return getUser(id)?.emails
     }
@@ -73,5 +44,38 @@ class QuipUserRepository {
      */
     fun getUserEmail(id: String): String? {
         return getUserEmails(id)?.maxByOrNull { it.length }
+    }
+
+    private companion object {
+        val extraEmailsPath: Path = Paths.get("quip_emails_extra.json")
+
+        fun QuipUserResource.withLowercaseId() = copy(id = id.lowercase())
+
+        fun getEmailByUserId(): Map<String, QuipUserResource> {
+            val userIdToEmail = gson()
+                .fromJson(Paths.get("quip_users_scim.json").readText(), JsonObject::class.java)
+                .getAsJsonArray("Resources")
+                .map { gson().fromJson(it, QuipUserResource::class.java).withLowercaseId() }
+                .associateBy { it.id.lowercase() }
+            val extraUserIdToEmail = if (extraEmailsPath.exists()) {
+                gson()
+                    .fromJson(extraEmailsPath.readText(), JsonObject::class.java)
+                    .asJsonObject
+                    .entrySet()
+                    .associate { entry ->
+                        val id = entry.key.lowercase()
+                        val nameAndEmail = entry.value.asString
+                        require(id !in userIdToEmail) { "Repeating id $id" }
+                        val parts = nameAndEmail.split(":")
+                        require(parts.size <= 2) { "Invalid name and email format for $id: $nameAndEmail" }
+                        val name = parts[0]
+                        val email = parts.getOrNull(1) ?: ""
+                        id to QuipUserResource(id, listOfNotNull(email), QuipUserName(name))
+                    }
+            } else {
+                emptyMap()
+            }
+            return userIdToEmail + extraUserIdToEmail
+        }
     }
 }
